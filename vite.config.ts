1ec4e1eb128a61438860602377def913c8186fa4
Aachal <aachalshrsth@gmail.com>
2025-12-28 19:30:38 +0545

    gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -14,4 +14,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/Portfolio/", 
 }));
